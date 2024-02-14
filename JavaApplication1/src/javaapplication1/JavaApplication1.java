f45d082c854b4c733ea1268752188ac35008ae56
JyotiPrajapati12 <142525127+JyotiPrajapati12@users.noreply.github.com>
2024-02-14 16:13:43 +0530

    Add files via upload

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author 93in
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
